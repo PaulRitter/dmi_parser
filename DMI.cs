33b6aa1cd3552a313d2e21efffaea4004e8e5afe
Paul <ritter.paul1@googlemail.com>
2020-04-17 10:32:21 +0200

    metadata read

---
+++ DMI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MetadataExtractor;
+
+namespace DMI_Parser
+{
+    public class DMI
+    {
+        public static DMI fromFile(String filepath){
+            String[] metadata = getMetadata(filepath);
+            foreach (var item in metadata)
+            {
+                Console.WriteLine(item);    
+            }
+
+ 
+            return null;
+        }
+
+        private static String[] getMetadata(String filepath){
+            var directories = ImageMetadataReader.ReadMetadata(filepath);
+
+            String[] dmi_metadata = null;
+            foreach (var directory in directories)
+            {
+                foreach (var tag in directory.Tags){
+                    if(tag.Name != "Textual Data")
+                        continue;
+                    dmi_metadata = tag.Description.Split(
+                        new[] { "\r\n", "\r", "\n" },
+                        StringSplitOptions.None
+                    );
+                    if(dmi_metadata[0] == "Description: # BEGIN DMI")
+                        return dmi_metadata;
+                    Console.WriteLine("["+tag.Name+"]: "+tag.Description);
+                }
+
+                if (directory.HasError)
+                {
+                    foreach (var error in directory.Errors)
+                        Console.WriteLine($"ERROR: {error}");
+                }
+            }
+
+            return dmi_metadata;
+        }
+    }
+}
