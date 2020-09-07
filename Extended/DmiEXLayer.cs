e2e5bffc3f3aba51067d1c8e9dd290b7886822e7
Paul <ritter.paul1@googlemail.com>
2020-09-07 13:08:08 +0200

    layer rubberducky

--- Extended/DmiEXLayer.cs
+++ Extended/DmiEXLayer.cs
@@ -11,93 +11,91 @@ namespace DMI_Parser.Extended
 {
     public class DmiEXLayer : IComparable, ICloneable
     {
-        public Bitmap Bitmap { get; private set; }
+        #region properties
 
-        private int _index;
         private bool _visible = true;
-
-        public event EventHandler IndexChanged;
-        public event EventHandler VisibilityChanged;
-        public event EventHandler ImageChanged;
-        public event EventHandler Changed;
-
         public bool Visible
         {
             get => _visible;
             set
             {
+                if (value == _visible) return;
+                
                 _visible = value;
                 VisibilityChanged?.Invoke(this, EventArgs.Empty);
             }
         }
+        public event EventHandler VisibilityChanged;
 
+        private int _index;
         public int Index
         {
             get => _index;
             set
             {
+                if (value == _index) return;
+                
                 _index = value;
                 IndexChanged?.Invoke(this, EventArgs.Empty);
             }
         }
+        public event EventHandler IndexChanged;
 
+        private Bitmap _bitmap;
+
+        #endregion
+
+        public event EventHandler ImageChanged;
+        public event EventHandler Changed;
+
+        
         public DmiEXLayer(Bitmap bitmap, int index)
         {
-            Bitmap = bitmap;
+            _bitmap = bitmap;
             _index = index;
-            IndexChanged += InvokeSomethingChanged;
-            VisibilityChanged += InvokeSomethingChanged;
-            ImageChanged += InvokeSomethingChanged;
+            IndexChanged += OnSomethingChanged;
+            VisibilityChanged += OnSomethingChanged;
+            ImageChanged += OnSomethingChanged;
         }
 
-        private void InvokeSomethingChanged(object sender, EventArgs e) => Changed?.Invoke(this, EventArgs.Empty);
+        private void OnSomethingChanged(object sender, EventArgs e) => Changed?.Invoke(this, EventArgs.Empty);
 
-        private void OnImageChanged()
-        {
-            ImageChanged?.Invoke(this, EventArgs.Empty);
-        }
+        private void OnImageChanged() => ImageChanged?.Invoke(this, EventArgs.Empty);
 
         public void SetPixel(Point p, Color c)
         {
             if(c == GetPixel(p)) return;
-            Bitmap.SetPixel(p.X, p.Y, c);
-            _bufferedImage = null;
+            
+            _bitmap.SetPixel(p.X, p.Y, c);
             OnImageChanged();
         }
 
-        public Color GetPixel(Point p) => Bitmap.GetPixel(p.X, p.Y);
-
-        private BitmapImage _bufferedImage;
-        public BitmapImage GetImage()
-        {
-            return _bufferedImage ??= BitmapUtils.Bitmap2BitmapImage(Bitmap);
-        }
+        public Color GetPixel(Point p) => _bitmap.GetPixel(p.X, p.Y);
 
         public void Resize(int width, int height)
         {
             MemoryStream imageStream = new MemoryStream();
             ImageFactory imgF = new ImageFactory()
-                .Load(Bitmap)
+                .Load(_bitmap)
                 .Resize(new ResizeLayer(new Size(width, height), ResizeMode.Crop, AnchorPosition.TopLeft))
                 .Format(new PngFormat())
                 .Save(imageStream);
             
-            Bitmap newBitmap = new Bitmap(imageStream);
+            _bitmap = new Bitmap(imageStream);
             imageStream.Close();
-            Bitmap = newBitmap;
             OnImageChanged();
         }
 
         public DmiEXState ToDmiExState(Dmi parent, string id)
         {
-            return new DmiEXState(parent, id, Bitmap);
+            return new DmiEXState(parent, id, _bitmap);
         }
 
         public void OverrideBitmap(Bitmap bm)
         {
-            if(bm.Width != Bitmap.Width || bm.Height != Bitmap.Height) throw new ArgumentException("Bitmap does not match Layer size");
+            if(bm.Width != _bitmap.Width || bm.Height != _bitmap.Height) throw new ArgumentException("Bitmap does not match Layer size");
 
-            Bitmap = bm;
+            _bitmap = bm;
             OnImageChanged();
         }
 
@@ -124,6 +122,6 @@ namespace DMI_Parser.Extended
                 throw new ArgumentException("Object is not a Layer");
         }
 
-        public object Clone() => new DmiEXLayer((Bitmap) Bitmap.Clone(), _index) {_visible = _visible};
+        public object Clone() => new DmiEXLayer((Bitmap) _bitmap.Clone(), _index) {_visible = _visible};
     }
 }
