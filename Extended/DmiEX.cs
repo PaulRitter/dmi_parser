7d943986e85cf12dd84b8f931eccbb2606a8ccf8
Paul <ritter.paul1@googlemail.com>
2020-09-18 19:35:26 +0200

    makes dmiex clonable

--- Extended/DmiEX.cs
+++ Extended/DmiEX.cs
@@ -3,7 +3,7 @@ using DMI_Parser.Raw;
 
 namespace DMI_Parser.Extended
 {
-    public class DmiEX : Dmi
+    public class DmiEX : Dmi, ICloneable
     {
         public DmiEX(float version, int width, int height) : base(version, width, height) {}
 
@@ -35,6 +35,17 @@ namespace DMI_Parser.Extended
             
             DmiEXState dmiState = new DmiEXState(this, images, raw);
             return AddState(dmiState);
+        }
+
+        public object Clone()
+        {
+            DmiEX newDmiex = new DmiEX(Version, Width, Height);
+            foreach (var state in States)
+            {
+                DmiEXState dmiExState = (DmiEXState) state;
+                newDmiex.AddState((DmiEXState) dmiExState.Clone());
+            }
+            return newDmiex;
         }
     }
 }
